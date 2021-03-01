8f34f272734757920324ce7cf4ed6c3db996b569
nevilamos <nevil.amos@gmail.com>
2021-03-01 04:07:31 +0000

    reformmated sync of inputs form AWS.s3

--- download_FAME_inputs_from_S3.R
+++ download_FAME_inputs_from_S3.R
@@ -1,9 +1,19 @@
 library(aws.s3)
-newDirPaths<-c("CustomCSV","FH_Outputs","HDMS","InputGeneralRasters","ReferenceShapeFiles","ReferenceTables","Results","rawFH","CustomCSV","AdHocPolygons")
+newDirPaths<-c("AdHocPolygons",
+               "CustomCSV",
+               "FH_Outputs",
+               "HDMS",
+               "InputGeneralRasters",
+               "rawFH",
+               "ReferenceShapeFiles",
+               "ReferenceTables",
+               "results"
+               )               
 for(mypath in newDirPaths){
 if (file.exists(mypath)){} else{
 
 dir.create(mypath,recursive = T)
+
 
 
 Sys.setenv("AWS_ACCESS_KEY_ID" = ,
@@ -11,5 +21,5 @@ Sys.setenv("AWS_ACCESS_KEY_ID" = ,
            "AWS_DEFAULT_REGION" = "ap-southeast-2")
 
 
-aws.s3::s3sync(path=paste0("./",mypath),bucket = "ecological-risk-analysis",prefix = paste0("FAME_FMR","/",path), check_region = F,direction = "download")}
+aws.s3::s3sync(path=paste0("./",mypath),bucket = "ecological-risk-analysis",prefix = paste0("FAME_FMR","/",mypath), check_region = F,direction = "download")}
 }
