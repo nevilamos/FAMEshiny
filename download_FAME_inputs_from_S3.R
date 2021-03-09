8462ac3693922fbecf8a5ba11e3d45ecfb968fea
nevilamos <nevil.amos@gmail.com>
2021-03-09 12:32:41 +1100

    fixed up s3 downlad problem with directory www

--- download_FAME_inputs_from_S3.R
+++ download_FAME_inputs_from_S3.R
@@ -9,7 +9,7 @@ newDirPaths<-c("AdHocPolygons",
                "ReferenceShapefiles",
                "ReferenceTables",
                "results",
-               "www"
+               "dummyname"# this name is a workaround for the shinyapp a directory and files ./www is required but s3sync will not open the directory so this name is used instead and renamed at the end of the process
 )               
 for(mypath in newDirPaths){
   if (file.exists(mypath)){} else{
@@ -23,15 +23,20 @@ for(mypath in newDirPaths){
       AWS_SECRET_ACCESS_KEY <- readline(prompt = "enter AWS_SECRET_ACCESS_KEY:")
     }
     
+
+    Sys.setenv("AWS_DEFAULT_REGION" = "ap-southeast-2")
+    
     
     aws.s3::s3sync(path=paste0("./",mypath),
                    bucket = "ecological-risk-analysis",
-                   prefix = paste0("FAME_FMR","/",mypath,"/"),
+                   prefix = paste0("FAME_FMR","/",mypath),
                    check_region = F,
                    direction = "download",
-                   key = AWS_ACCESS_KEY_ID,
-                   secret = AWS_SECRET_ACCESS_KEY,
-                   region = "ap-southeast-2")
+                   key=AWS_ACCESS_KEY_ID,
+                   secret =AWS_SECRET_ACCESS_KEY)
   }
 }
+file.rename("dummyname","www")
+dir.create("dummyname")
 rm(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
+
