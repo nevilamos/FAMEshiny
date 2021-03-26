a38352f412ce6a17cc91d506cc229e99b8305b60
nevilamos <nevil.amos@gmail.com>
2021-03-26 15:46:16 +1100

    moved check for installed packages and files to downlaod from s3 into separate file "installationCheck.R so it is availble for use from shiny or running scripts from console

--- global.r
+++ global.r
@@ -3,69 +3,13 @@ options(shiny.reactlog = TRUE)
 
 
 options(stringsAsFactors = F)
-
-## If a package is installed, it will be loaded. If any 
-## are not, the missing package(s) will be installed 
-## from CRAN and then loaded.
-
-## First specify the packages of interest
-packages = c("aws.s3",
-             "dashboardthemes",
-             "doParallel",
-             "dplyr",
-             "fasterize",
-             "foreach",
-             "gdalUtils",
-             "ggplot2",
-             "knitr",
-             "Matrix.utils",
-             "nloptr",
-             "plotly",
-             "qs",
-             "raster",
-             "readxl",
-             "Rfast",
-             "rmarkdown",
-             "rlang",
-             "sf",
-             "shiny",
-             "shinycssloaders",
-             "shinydashboard",
-             "shinyFiles",
-             "shinyjs",
-             "stringr",
-             "tabularaster",
-             "tibble",
-             "tidyr",
-             "tools")
-
-## Now load or install&load all packages from cran
-package.check <- lapply(
-  packages,
-  FUN = function(x) {
-    if (!require(x, character.only = TRUE)) {
-      install.packages(x, dependencies = TRUE)
-      library(x, character.only = TRUE)
-    }
-  }
-)
-
-#check that FAMEFMR package is installed and get it from github if not.
-if(require("FAMEFMR")==FALSE){
-  devtools::install_github("nevilamos/FAMEFMR")
-  }
+source("installationCheck.R")
 
 
-#run once only to copy all the input files from AWS bucket-----------------------------
-source("download_FAME_inputs_from_S3.R")
-#for debugging------------------
-options(warn = -1)
 
-
-#library(shinythemes)
 
 #get version of FAMEFMR in use and set app version 
-versionDate = "Version 1.9.1 March 10 2021"
+versionDate = "Version 1.9.2 March 26 2021"
 versionFAMEFMR = paste ("R", getRversion(),"FAMEFMR",packageVersion("FAMEFMR"))
 
 
---
+++ installationCheck.R
@@ -0,0 +1,57 @@
+
+## If a package is installed, it will be loaded. If any 
+## are not, the missing package(s) will be installed 
+## from CRAN and then loaded.
+
+## First specify the packages of interest
+packages = c("aws.s3",
+             "dashboardthemes",
+             "doParallel",
+             "dplyr",
+             "fasterize",
+             "foreach",
+             "gdalUtils",
+             "ggplot2",
+             "knitr",
+             "Matrix.utils",
+             "nloptr",
+             "plotly",
+             "qs",
+             "raster",
+             "readxl",
+             "Rfast",
+             "rmarkdown",
+             "rlang",
+             "sf",
+             "shiny",
+             "shinycssloaders",
+             "shinydashboard",
+             "shinyFiles",
+             "shinyjs",
+             "stringr",
+             "tabularaster",
+             "tibble",
+             "tidyr",
+             "tools")
+
+## Now load or install&load all packages from cran
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+#check that FAMEFMR package is installed and get it from github if not.
+if(require("FAMEFMR")==FALSE){
+  devtools::install_github("nevilamos/FAMEFMR")
+}
+
+
+#run once only to copy all the input files from AWS bucket-----------------------------
+source("download_FAME_inputs_from_S3.R")
+#for debugging------------------
+options(warn = -1)
