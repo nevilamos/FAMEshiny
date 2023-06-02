742a1fcb2db5aa72405b67ff0d754e63c7c23c5a
nevilamos <nevil.amos@gmail.com>
2023-06-02 13:14:13 +1000

    edited list of required packages in installation check and updated to FAME4 in downlad files from s3

--- download_FAME_inputs_from_S3.R
+++ download_FAME_inputs_from_S3.R
@@ -15,33 +15,48 @@ newDirPaths<-c("AdHocPolygons",
 # 
 # Sys.setenv("AWS_DEFAULT_REGION" = "ap-southeast-2")
 # 
+# library(aws.s3)
+newDirPaths<-c("AdHocPolygons",
+               "CustomCSV",
+               "FH_Outputs",
+               "HDMS",
+               "InputGeneralRasters",
+               "PUPolygons",
+               "rawFH",
+               "ReferenceShapefiles",
+               "ReferenceTables",
+               "results",
+               "www"# 
+)
+# 
+# 
+# Sys.setenv("AWS_DEFAULT_REGION" = "ap-southeast-2")
+# 
 # 
 for(mypath in newDirPaths){
-
+  
   if (file.exists(mypath)){
-
+    
   } else {
     dir.create(mypath,recursive = T)
-
-   }
-#   
-#   
+    
+  }
+  #   
+  #   
   
 }
-if(!file.exists("FAME_inputfiles.zip")){
-  download.file("https://ecological-risk-analysis.s3-ap-southeast-2.amazonaws.com/FAME_FMR/FAME4_inputfiles.zip","FAME_inputfiles.zip")
+if(!file.exists("FAME4_inputfiles.zip")){
+  download.file("https://ecological-risk-analysis.s3-ap-southeast-2.amazonaws.com/FAME_FMR/FAME4_inputfiles.zip","FAME4_inputfiles.zip")
   
   if (.Platform$OS.type == "unix"){
-    system("unzip -o ./FAME_inputfiles.zip && cp -rl ./FAME_inputfiles ./  && rm -r ./FAME_inputfiles")
+    system("unzip -o ./FAME4_inputfiles.zip && cp -r FAME4_inputfiles/. . ")
   }else{
     unzip("./FAME_inputfiles.zip",exdir = ".")
-    system("xcopy .\\FAME_inputfiles\ . /E/H/y")
-    unlink("FAME_inputfiles",recursive=TRUE)
+    system("xcopy .\\FAME4_inputfiles\ . /E/H/y")
+    
   }
-  
+  unlink("FAME4_inputfiles",recursive=TRUE)
 }
-
-
 
 
 
